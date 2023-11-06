024f9691e71ed0a177897639b1cb801fc61abcff
fowefue <149468705+fowefue@users.noreply.github.com>
2023-11-07 01:00:34 +0900

    first commit

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "myProject.h"
+int main(viod)
+{
+    printf ("Chief name is KIMSEUNGYU\n");
+    printMyName1();
+    printMyName2();
+    printMyName3();
+    printMyName4();
+}
